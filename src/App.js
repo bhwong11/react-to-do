eca80c93459ddb52271e498d55d49ca696ab1749
bhwong11 <user@email.com>
2021-08-31 18:03:35 -0400

    added styles for sprint 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 
+import Header from './components/Header';
+import Routes from './config/routes';
 
 function App() {
   return (
     <div className="container">
       <h1>Hello World!</h1>
+      <Header/>
+      <Routes/>
     </div>
   );
 }
